bbce2d584f88f99729ca5f9bdb706a3fd0d2216e
Nadim <shantalifeins@gmail.com>
2026-09-17 10:53:10 +0600

    fix(hotfix): set trust proxy in express to prevent rate limiter crash on live reverse proxy

--- server.ts
+++ server.ts
@@ -796,6 +796,7 @@ async function startServer() {
     }
   });
 
+  app.set('trust proxy', 1);
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
